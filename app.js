1c09c800c4c0d84fc99ae9aaac67c671f0d15034
Tan Mai <tanmai833@gmail.com>
2024-12-06 18:36:21 +0700

    add actions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Xin chào đây là thử nghiệm ci-cd');
 });
 
 app.listen(port, () => {

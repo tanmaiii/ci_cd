ee18a84777a202e4e0cd09771f65f742e188988e
Tan Mai <tanmai833@gmail.com>
2024-12-06 21:34:57 +0700

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Xin chào đây là thử nghiệm ci-cd');
+  res.send('Có thay đổi nè');
 });
 
 app.listen(port, () => {
